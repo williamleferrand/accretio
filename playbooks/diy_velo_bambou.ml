78d16478424f2f5776736c0c010c22cf8e5d1e87
William Le Ferrand <william@sumologic.com>
2016-02-16 18:51:22 -0800

    adding reminders

--- playbooks/diy_velo_bambou.ml
+++ playbooks/diy_velo_bambou.ml
@@ -73,23 +73,30 @@ let extract_all_emails context message =
       [] -> return `None
     | _ as members ->
       lwt _ =
+        match_lwt $message(message)->attachments with
+          [] -> return_unit
+        | _ -> context.set ~key:(key_original_email run_id) ~value:(Ys_uid.to_string message) in
+      lwt _ =
         Lwt_list.iter_s
-          (fun member -> context.tag_member ~member ~tags:[ tag_candidate run_id ])
+          (fun member ->
+             lwt _ = context.tag_member ~member ~tags:[ tag_candidate run_id ] in
+             lwt _ =
+               context.set_timer
+                 ~duration:(Calendar.Period.lmake ~second:10 ())
+                 (`AskCandidate (run_id, member))
+             in
+             return_unit)
           members
       in
       lwt _ =
         context.reply_to
           ~message
           ~content:[
-            pcdata "Merci, je vais prendre contact avec ces personnes!"
+            pcdata "Merci, je vais prendre contact avec ces personnes (" ; pcdata (string_of_int (List.length members)) ; pcdata " en tout)!"
           ]
         ()
       in
-      lwt _ =
-        match_lwt $message(message)->attachments with
-          [] -> return_unit
-        | _ -> context.set ~key:(key_original_email run_id) ~value:(Ys_uid.to_string message) in
-      return (`AskCandidatesForTheirOpinion run_id)
+      return (`None)
 
 let tag_interested = sprintf "interested%Ld"
 let tag_not_interested = sprintf "notinterested%Ld"
@@ -113,8 +120,9 @@ let send_summary context message =
 
 let tag_already_asked = sprintf "alreadyasked%Ld"
 
-let ask_candidates_for_their_opinion context run_id =
-  lwt candidates = context.search_members ~query:(sprintf "%s -%s" (tag_candidate run_id) (tag_already_asked run_id)) () in
+let tag_timer = sprintf "reminder%Ld%d"
+
+let ask_candidate context (run_id, member) =
   lwt attachments =
     match_lwt context.get ~key:(key_original_email run_id) with
       None -> return []
@@ -122,34 +130,49 @@ let ask_candidates_for_their_opinion context run_id =
       let message = Ys_uid.of_string message in
       $message(message)->attachments
   in
+  lwt salutations = salutations_fr member in
+  match_lwt
+    context.message_member
+      ~member
+      ~subject:"Construire un vélo en bambou sur mesure"
+      ~data:(data_run_id run_id)
+      ~attachments
+      ~content:[
+        salutations ; br () ;
+        br () ;
+        pcdata "Savez vous que l'on peut fabriquer un cadre de vélo à partir de bambou?" ; br () ;
+        br () ;
+        pcdata "Maintenant que j'ai construit le mien j'aimerais partager cette experience avec d'autres personnes en organisant une commande groupée des matériaux et un atelier de montage." ; br () ;
+        ul [
+          li [ b [ pcdata "Où?" ] ; pcdata " ça se passerait à Paris, dans les locaux du Langevinium" ] ;
+          li [ b [ pcdata "Combien?" ] ; pcdata " le cadre seul (à prix coûtant) devrait revenir à environ 90 euros" ] ;
+          li [ b [ pcdata "Quand?" ] ; pcdata " à discuter selon les disponibilités des personnes intéréssées" ] ;
+        ] ;
+        pcdata "Est ce que ça vous tenterait? Aucun engagement pour le moment, dites moi juste si vous voulez rester dans la boucle et j'essaierai d'organiser tout ça en fonction des réponses." ; br () ;
+      ]
+      () with
+    None -> return `None
+  | Some message ->
+
+    lwt _ = context.tag_member ~member ~tags:[ tag_already_asked run_id ] in
+    lwt _ =
+      context.set_timer
+        ~label:(tag_timer run_id member)
+        ~duration:(Calendar.Period.lmake ~day:2 ())
+        (`RemindCandidate (run_id, member, message))
+    in
+    return `None
+
+let remind_candidate context (run_id, member, message) =
+  lwt _ = context.cancel_timers ~query:(tag_timer run_id member) in
   lwt _ =
-    Lwt_list.iter_s
-      (fun member ->
-         lwt salutations = salutations_fr member in
-         lwt _ =
-           context.message_member
-             ~member
-             ~subject:"Construire un vélo en bambou sur mesure"
-             ~data:(data_run_id run_id)
-             ~attachments
-             ~content:[
-               salutations ; br () ;
-               br () ;
-               pcdata "Savez vous que l'on peut fabriquer un cadre de vélo à partir de bambou?" ; br () ;
-               br () ;
-               pcdata "Maintenant que j'ai construit le mien j'aimerais partager cette experience avec d'autres personnes en organisant une commande groupée des matériaux et un atelier de montage." ; br () ;
-               ul [
-                 li [ b [ pcdata "Où?" ] ; pcdata " ça se passerait à Paris, dans les locaux du Langevinium" ] ;
-                 li [ b [ pcdata "Combien?" ] ; pcdata " le cadre seul (à prix coûtant) devrait revenir à environ 90 euros" ] ;
-                 li [ b [ pcdata "Quand?" ] ; pcdata " à discuter selon les disponibilités des personnes intéréssées" ] ;
-               ] ;
-               pcdata "Est ce que ça vous tenterait? Aucun engagement pour le moment, dites moi juste si vous voulez rester dans la boucle et j'essaierai d'organiser tout ça en fonction des réponses." ; br () ;
-             ]
-             ()
-         in
-         lwt _ = context.tag_member ~member ~tags:[ tag_already_asked run_id ] in
-         return_unit)
-      candidates
+    context.reply_to
+      ~message
+      ~data:(data_run_id run_id)
+      ~content:[
+        pcdata "Bonjour, avez vous vu mon message concernant le vélo en bambou? Etes vous intéressé par en savoir plus?"
+      ]
+      ()
   in
   return `None
 
@@ -159,6 +182,7 @@ let mark_interested context message =
   | Some run_id ->
     lwt member = context.get_message_sender ~message in
     lwt _ = context.tag_member ~member ~tags:[ tag_interested run_id ] in
+    lwt _ = context.cancel_timers ~query:(tag_timer run_id member) in
     lwt _ =
       context.reply_to
         ~message
@@ -174,6 +198,7 @@ let mark_not_interested context message =
   | Some run_id ->
     lwt member = context.get_message_sender ~message in
     lwt _ = context.tag_member ~member ~tags:[ tag_not_interested run_id ] in
+    lwt _ = context.cancel_timers ~query:(tag_timer run_id member) in
     lwt _ =
       context.reply_to
         ~message
@@ -187,8 +212,10 @@ let mark_not_interested context message =
 
 PLAYBOOK
 
-*start<forward> ~> `Message of email ~> extract_all_emails ~> `AskCandidatesForTheirOpinion of int64 ~> ask_candidates_for_their_opinion
+*start<forward> ~> `Message of email ~> extract_all_emails ~> `AskCandidate of (int64 * int) ~> ask_candidate ~> `RemindCandidate of (int64 * int * int) ~> remind_candidate
  start ~> `SendSummary of email ~> send_summary
 
- ask_candidates_for_their_opinion ~> `NotInterested of email ~> mark_not_interested
- ask_candidates_for_their_opinion ~> `Interested of email ~> mark_interested
+ ask_candidate ~> `NotInterested of email ~> mark_not_interested
+ ask_candidate ~> `Interested of email ~> mark_interested
+ remind_candidate ~> `NotInterested of email ~> mark_not_interested
+ remind_candidate ~> `Interested of email ~> mark_interested
